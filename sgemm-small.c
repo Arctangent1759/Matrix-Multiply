db3a5364f25cba5d7787885c3d342e11eca72e54
Class Account <cs61c-hp@hive25.CS.Berkeley.EDU>
2013-07-24 09:41:48 -0700

    untested implementation of segmm-small

--- sgemm-small.c
+++ sgemm-small.c
@@ -1,9 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <emmintrin.h>
 void sgemm( int m_a, int n_a, float *A, float *B, float *C ) {
-  for( int i = 0; i < m_a; i++ ) {
-    for( int k = 0; k < n_a; k++ ) {
-      for( int j = 0; j < m_a; j++ ) {
-	C[i+j*m_a] += A[i+k*m_a] * B[j+k*m_a];
-      }
-    }
+
+  register int a_column,a_index=0, b_index=0, max_index=m_a*n_a
+  _m128 a_vec,b_vec,c_vec;
+
+  for (; a_column < max_index; a_column+=m_a){
+	for (; b_index < max_index; b_index+=1){
+	  b_vec = _mm_set_ps1(B[b_index]);
+	  for (a_index = 0 ; a_index < m_a; a_index+=4){
+		a_vec = _mm_loadu_ps(A+a_column+a_index);
+		c_vec = _mm_loadu_ps(A+a_column+a_index);
+		_mm_add_ps(c_vec,_mm_mul_ps(a_vec,b_vec));
+		_mm_storeu_ps(C+a_column,c_vec);
+	  }
+	}
   }
+
+
+
+
+
+  register int a_column=0,a_index=0,b_index=0,max_index=m_a*n_a,a_vec,b_vec,c_vec;
+  for (; a_column<max_index; a_column+=m_a){
+	a_vec = _mm_loadu_ps(A+a_column);
+	c_vec = _mm_loadu_ps(C+a_column);
+	for (b_index=0; b_index<max_index; b_index++){
+	  b_vec = _mm_set_ps1(B[b_index]);
+	  _mm_add_ps(c_vec,_mm_mul_ps(a_vec,b_vec));
+	}
+    _mm_storeu_ps(C+a_column,c_vec);
+  }
+
 }
