2c0fe7ea08b13eb3f74b6c957cc4fcad77195578
Class Account <cs61c-hp@hive25.CS.Berkeley.EDU>
2013-07-24 09:42:38 -0700

    cleanup

--- sgemm-small.c
+++ sgemm-small.c
@@ -17,20 +17,4 @@ void sgemm( int m_a, int n_a, float *A, float *B, float *C ) {
 	  }
 	}
   }
-
-
-
-
-
-  register int a_column=0,a_index=0,b_index=0,max_index=m_a*n_a,a_vec,b_vec,c_vec;
-  for (; a_column<max_index; a_column+=m_a){
-	a_vec = _mm_loadu_ps(A+a_column);
-	c_vec = _mm_loadu_ps(C+a_column);
-	for (b_index=0; b_index<max_index; b_index++){
-	  b_vec = _mm_set_ps1(B[b_index]);
-	  _mm_add_ps(c_vec,_mm_mul_ps(a_vec,b_vec));
-	}
-    _mm_storeu_ps(C+a_column,c_vec);
-  }
-
 }
