4840959b6fc33b9422d2b5d67ed7016701de123c
Class Account <cs61c-hp@hive25.CS.Berkeley.EDU>
2013-07-28 16:41:00 -0700

    part 1

--- sgemm-small.c
+++ sgemm-small.c
@@ -161,12 +161,13 @@ void sgemm( int m_a, int n_a, float *A, float *B, float *C ) {
 		  c_vec4=_mm_add_ps(_mm_mul_ps(a_vec,b_vec),c_vec4);
 
 		  /**
-		   * For the brave souls who get this far: You are the chosen ones,
+		   * "For the brave souls who get this far: You are the chosen ones,
 		   * the valiant knights of programming who toil away, without rest,
 		   * fixing our most awful code. To you, true saviors, kings of men,
 		   * I say this: never gonna give you up, never gonna let you down,
 		   * never gonna run around and desert you. Never gonna make you cry,
-		   * never gonna say goodbye. Never gonna tell a lie and hurt you.
+		   * never gonna say goodbye. Never gonna tell a lie and hurt you."
+		   *                                                  --Someone on Stack Overflow
 		   */
 
 
@@ -230,13 +231,236 @@ void sgemm( int m_a, int n_a, float *A, float *B, float *C ) {
 		_mm_storeu_ps(C+(col_index_B+8)*m_a+row_index_A,c_vec9);
 	  }
 	}
+  }else if (m_a%3==0){
+	register int row_index_A, col_index_B=0, iterator_index_AB, max_size=m_a*n_a;
+	const register int col_remainder = m_a%(12), row_remainder = n_a%EDGE_SIZE_ROW;
+	register float c_dummy;
+	register __m128 a_vec, b_vec, c_vec1, c_vec2, c_vec3;
+
+	for (; col_index_B < m_a; col_index_B+=3){ //make sure index_B is the index of the beginning of each column of B
+	  for (row_index_A=0; row_index_A+11 < m_a; row_index_A+=12){ //Iterate accross each 4xm_a horizontal bock of A
+		c_vec1 = _mm_setzero_ps(); //Load C_Vec into memory
+		c_vec2 = _mm_setzero_ps(); //Load C_Vec into memory
+		c_vec3 = _mm_setzero_ps(); //Load C_Vec into memory
+		for (iterator_index_AB=0; iterator_index_AB+EDGE_SIZE_ROW-1 < n_a; iterator_index_AB+=EDGE_SIZE_ROW){ //Reduce each block into c_vec
+		  a_vec = _mm_loadu_ps(A+iterator_index_AB*m_a+row_index_A);
+		  b_vec = _mm_set_ps1(B[iterator_index_AB*m_a+col_index_B]);
+		  c_vec1=_mm_add_ps(_mm_mul_ps(a_vec,b_vec),c_vec1);
+
+		  a_vec = _mm_loadu_ps(A+(iterator_index_AB+1)*m_a+row_index_A);
+		  b_vec = _mm_set_ps1(B[(iterator_index_AB+1)*m_a+col_index_B]);
+		  c_vec1=_mm_add_ps(_mm_mul_ps(a_vec,b_vec),c_vec1);
+
+		  a_vec = _mm_loadu_ps(A+(iterator_index_AB+2)*m_a+row_index_A);
+		  b_vec = _mm_set_ps1(B[(iterator_index_AB+2)*m_a+col_index_B]);
+		  c_vec1=_mm_add_ps(_mm_mul_ps(a_vec,b_vec),c_vec1);
+
+		  a_vec = _mm_loadu_ps(A+iterator_index_AB*m_a+row_index_A+4);
+		  b_vec = _mm_set_ps1(B[iterator_index_AB*m_a+col_index_B]);
+		  c_vec2=_mm_add_ps(_mm_mul_ps(a_vec,b_vec),c_vec2);
+
+		  a_vec = _mm_loadu_ps(A+(iterator_index_AB+1)*m_a+row_index_A+4);
+		  b_vec = _mm_set_ps1(B[(iterator_index_AB+1)*m_a+col_index_B]);
+		  c_vec2=_mm_add_ps(_mm_mul_ps(a_vec,b_vec),c_vec2);
+
+		  a_vec = _mm_loadu_ps(A+(iterator_index_AB+2)*m_a+row_index_A+4);
+		  b_vec = _mm_set_ps1(B[(iterator_index_AB+2)*m_a+col_index_B]);
+		  c_vec2=_mm_add_ps(_mm_mul_ps(a_vec,b_vec),c_vec2);
+
+		  a_vec = _mm_loadu_ps(A+iterator_index_AB*m_a+row_index_A+8);
+		  b_vec = _mm_set_ps1(B[iterator_index_AB*m_a+col_index_B]);
+		  c_vec3=_mm_add_ps(_mm_mul_ps(a_vec,b_vec),c_vec3);
+
+		  a_vec = _mm_loadu_ps(A+(iterator_index_AB+1)*m_a+row_index_A+8);
+		  b_vec = _mm_set_ps1(B[(iterator_index_AB+1)*m_a+col_index_B]);
+		  c_vec3=_mm_add_ps(_mm_mul_ps(a_vec,b_vec),c_vec3);
+
+		  a_vec = _mm_loadu_ps(A+(iterator_index_AB+2)*m_a+row_index_A+8);
+		  b_vec = _mm_set_ps1(B[(iterator_index_AB+2)*m_a+col_index_B]);
+		  c_vec3=_mm_add_ps(_mm_mul_ps(a_vec,b_vec),c_vec3);
+
+		}
+		if (row_remainder != 0){
+		  for (iterator_index_AB=row_remainder; iterator_index_AB != 0; iterator_index_AB--){
+			a_vec = _mm_loadu_ps(A+(n_a-iterator_index_AB)*m_a+row_index_A);
+			b_vec = _mm_set_ps1(B[(n_a-iterator_index_AB)*m_a+col_index_B]);
+			c_vec1=_mm_add_ps(_mm_mul_ps(a_vec,b_vec),c_vec1);
+
+			a_vec = _mm_loadu_ps(A+(n_a-iterator_index_AB)*m_a+row_index_A+4);
+			b_vec = _mm_set_ps1(B[(n_a-iterator_index_AB)*m_a+col_index_B]);
+			c_vec2=_mm_add_ps(_mm_mul_ps(a_vec,b_vec),c_vec2);
+
+			a_vec = _mm_loadu_ps(A+(n_a-iterator_index_AB)*m_a+row_index_A+8);
+			b_vec = _mm_set_ps1(B[(n_a-iterator_index_AB)*m_a+col_index_B]);
+			c_vec3=_mm_add_ps(_mm_mul_ps(a_vec,b_vec),c_vec3);
+		  }
+		}
+		_mm_storeu_ps(C+col_index_B*m_a+row_index_A,c_vec1);
+		_mm_storeu_ps(C+col_index_B*m_a+row_index_A+4,c_vec2);
+		_mm_storeu_ps(C+col_index_B*m_a+row_index_A+8,c_vec3);
+	  }
+	  if (col_remainder != 0) {
+		for (row_index_A = col_remainder;row_index_A != 0; row_index_A--){	
+		  c_dummy = 0;
+		  for (iterator_index_AB=0; iterator_index_AB < n_a; iterator_index_AB+=1){ //Reduce each block into c_vec
+			c_dummy += A[iterator_index_AB*m_a+m_a-row_index_A] * B[iterator_index_AB*m_a+col_index_B];  
+		  }
+		  C[col_index_B*m_a+m_a-row_index_A] = c_dummy; 
+		}
+	  } 
+
+
+	  for (row_index_A=0; row_index_A+11 < m_a; row_index_A+=12){ //Iterate accross each 4xm_a horizontal bock of A
+		c_vec1 = _mm_setzero_ps(); //Load C_Vec into memory
+		c_vec2 = _mm_setzero_ps(); //Load C_Vec into memory
+		c_vec3 = _mm_setzero_ps(); //Load C_Vec into memory
+		for (iterator_index_AB=0; iterator_index_AB+EDGE_SIZE_ROW-1 < n_a; iterator_index_AB+=EDGE_SIZE_ROW){ //Reduce each block into c_vec
+		  a_vec = _mm_loadu_ps(A+iterator_index_AB*m_a+row_index_A);
+		  b_vec = _mm_set_ps1(B[iterator_index_AB*m_a+(col_index_B+1)]);
+		  c_vec1=_mm_add_ps(_mm_mul_ps(a_vec,b_vec),c_vec1);
+
+		  a_vec = _mm_loadu_ps(A+(iterator_index_AB+1)*m_a+row_index_A);
+		  b_vec = _mm_set_ps1(B[(iterator_index_AB+1)*m_a+(col_index_B+1)]);
+		  c_vec1=_mm_add_ps(_mm_mul_ps(a_vec,b_vec),c_vec1);
+
+		  a_vec = _mm_loadu_ps(A+(iterator_index_AB+2)*m_a+row_index_A);
+		  b_vec = _mm_set_ps1(B[(iterator_index_AB+2)*m_a+(col_index_B+1)]);
+		  c_vec1=_mm_add_ps(_mm_mul_ps(a_vec,b_vec),c_vec1);
+
+		  a_vec = _mm_loadu_ps(A+iterator_index_AB*m_a+row_index_A+4);
+		  b_vec = _mm_set_ps1(B[iterator_index_AB*m_a+(col_index_B+1)]);
+		  c_vec2=_mm_add_ps(_mm_mul_ps(a_vec,b_vec),c_vec2);
+
+		  a_vec = _mm_loadu_ps(A+(iterator_index_AB+1)*m_a+row_index_A+4);
+		  b_vec = _mm_set_ps1(B[(iterator_index_AB+1)*m_a+(col_index_B+1)]);
+		  c_vec2=_mm_add_ps(_mm_mul_ps(a_vec,b_vec),c_vec2);
+
+		  a_vec = _mm_loadu_ps(A+(iterator_index_AB+2)*m_a+row_index_A+4);
+		  b_vec = _mm_set_ps1(B[(iterator_index_AB+2)*m_a+(col_index_B+1)]);
+		  c_vec2=_mm_add_ps(_mm_mul_ps(a_vec,b_vec),c_vec2);
+
+		  a_vec = _mm_loadu_ps(A+iterator_index_AB*m_a+row_index_A+8);
+		  b_vec = _mm_set_ps1(B[iterator_index_AB*m_a+(col_index_B+1)]);
+		  c_vec3=_mm_add_ps(_mm_mul_ps(a_vec,b_vec),c_vec3);
+
+		  a_vec = _mm_loadu_ps(A+(iterator_index_AB+1)*m_a+row_index_A+8);
+		  b_vec = _mm_set_ps1(B[(iterator_index_AB+1)*m_a+(col_index_B+1)]);
+		  c_vec3=_mm_add_ps(_mm_mul_ps(a_vec,b_vec),c_vec3);
+
+		  a_vec = _mm_loadu_ps(A+(iterator_index_AB+2)*m_a+row_index_A+8);
+		  b_vec = _mm_set_ps1(B[(iterator_index_AB+2)*m_a+(col_index_B+1)]);
+		  c_vec3=_mm_add_ps(_mm_mul_ps(a_vec,b_vec),c_vec3);
+
+		}
+		if (row_remainder != 0){
+		  for (iterator_index_AB=row_remainder; iterator_index_AB != 0; iterator_index_AB--){
+			a_vec = _mm_loadu_ps(A+(n_a-iterator_index_AB)*m_a+row_index_A);
+			b_vec = _mm_set_ps1(B[(n_a-iterator_index_AB)*m_a+(col_index_B+1)]);
+			c_vec1=_mm_add_ps(_mm_mul_ps(a_vec,b_vec),c_vec1);
+
+			a_vec = _mm_loadu_ps(A+(n_a-iterator_index_AB)*m_a+row_index_A+4);
+			b_vec = _mm_set_ps1(B[(n_a-iterator_index_AB)*m_a+(col_index_B+1)]);
+			c_vec2=_mm_add_ps(_mm_mul_ps(a_vec,b_vec),c_vec2);
+
+			a_vec = _mm_loadu_ps(A+(n_a-iterator_index_AB)*m_a+row_index_A+8);
+			b_vec = _mm_set_ps1(B[(n_a-iterator_index_AB)*m_a+(col_index_B+1)]);
+			c_vec3=_mm_add_ps(_mm_mul_ps(a_vec,b_vec),c_vec3);
+		  }
+		}
+		_mm_storeu_ps(C+(col_index_B+1)*m_a+row_index_A,c_vec1);
+		_mm_storeu_ps(C+(col_index_B+1)*m_a+row_index_A+4,c_vec2);
+		_mm_storeu_ps(C+(col_index_B+1)*m_a+row_index_A+8,c_vec3);
+	  }
+	  if (col_remainder != 0) {
+		for (row_index_A = col_remainder;row_index_A != 0; row_index_A--){	
+		  c_dummy = 0;
+		  for (iterator_index_AB=0; iterator_index_AB < n_a; iterator_index_AB+=1){ //Reduce each block into c_vec
+			c_dummy += A[iterator_index_AB*m_a+m_a-row_index_A] * B[iterator_index_AB*m_a+(col_index_B+1)];  
+		  }
+		  C[(col_index_B+1)*m_a+m_a-row_index_A] = c_dummy; 
+		}
+	  } 
+
+	  for (row_index_A=0; row_index_A+11 < m_a; row_index_A+=12){ //Iterate accross each 4xm_a horizontal bock of A
+		c_vec1 = _mm_setzero_ps(); //Load C_Vec into memory
+		c_vec2 = _mm_setzero_ps(); //Load C_Vec into memory
+		c_vec3 = _mm_setzero_ps(); //Load C_Vec into memory
+		for (iterator_index_AB=0; iterator_index_AB+EDGE_SIZE_ROW-1 < n_a; iterator_index_AB+=EDGE_SIZE_ROW){ //Reduce each block into c_vec
+		  a_vec = _mm_loadu_ps(A+iterator_index_AB*m_a+row_index_A);
+		  b_vec = _mm_set_ps1(B[iterator_index_AB*m_a+(col_index_B+2)]);
+		  c_vec1=_mm_add_ps(_mm_mul_ps(a_vec,b_vec),c_vec1);
+
+		  a_vec = _mm_loadu_ps(A+(iterator_index_AB+1)*m_a+row_index_A);
+		  b_vec = _mm_set_ps1(B[(iterator_index_AB+1)*m_a+(col_index_B+2)]);
+		  c_vec1=_mm_add_ps(_mm_mul_ps(a_vec,b_vec),c_vec1);
+
+		  a_vec = _mm_loadu_ps(A+(iterator_index_AB+2)*m_a+row_index_A);
+		  b_vec = _mm_set_ps1(B[(iterator_index_AB+2)*m_a+(col_index_B+2)]);
+		  c_vec1=_mm_add_ps(_mm_mul_ps(a_vec,b_vec),c_vec1);
+
+		  a_vec = _mm_loadu_ps(A+iterator_index_AB*m_a+row_index_A+4);
+		  b_vec = _mm_set_ps1(B[iterator_index_AB*m_a+(col_index_B+2)]);
+		  c_vec2=_mm_add_ps(_mm_mul_ps(a_vec,b_vec),c_vec2);
+
+		  a_vec = _mm_loadu_ps(A+(iterator_index_AB+1)*m_a+row_index_A+4);
+		  b_vec = _mm_set_ps1(B[(iterator_index_AB+1)*m_a+(col_index_B+2)]);
+		  c_vec2=_mm_add_ps(_mm_mul_ps(a_vec,b_vec),c_vec2);
+
+		  a_vec = _mm_loadu_ps(A+(iterator_index_AB+2)*m_a+row_index_A+4);
+		  b_vec = _mm_set_ps1(B[(iterator_index_AB+2)*m_a+(col_index_B+2)]);
+		  c_vec2=_mm_add_ps(_mm_mul_ps(a_vec,b_vec),c_vec2);
+
+		  a_vec = _mm_loadu_ps(A+iterator_index_AB*m_a+row_index_A+8);
+		  b_vec = _mm_set_ps1(B[iterator_index_AB*m_a+(col_index_B+2)]);
+		  c_vec3=_mm_add_ps(_mm_mul_ps(a_vec,b_vec),c_vec3);
+
+		  a_vec = _mm_loadu_ps(A+(iterator_index_AB+1)*m_a+row_index_A+8);
+		  b_vec = _mm_set_ps1(B[(iterator_index_AB+1)*m_a+(col_index_B+2)]);
+		  c_vec3=_mm_add_ps(_mm_mul_ps(a_vec,b_vec),c_vec3);
+
+		  a_vec = _mm_loadu_ps(A+(iterator_index_AB+2)*m_a+row_index_A+8);
+		  b_vec = _mm_set_ps1(B[(iterator_index_AB+2)*m_a+(col_index_B+2)]);
+		  c_vec3=_mm_add_ps(_mm_mul_ps(a_vec,b_vec),c_vec3);
+
+		}
+		if (row_remainder != 0){
+		  for (iterator_index_AB=row_remainder; iterator_index_AB != 0; iterator_index_AB--){
+			a_vec = _mm_loadu_ps(A+(n_a-iterator_index_AB)*m_a+row_index_A);
+			b_vec = _mm_set_ps1(B[(n_a-iterator_index_AB)*m_a+(col_index_B+2)]);
+			c_vec1=_mm_add_ps(_mm_mul_ps(a_vec,b_vec),c_vec1);
+
+			a_vec = _mm_loadu_ps(A+(n_a-iterator_index_AB)*m_a+row_index_A+4);
+			b_vec = _mm_set_ps1(B[(n_a-iterator_index_AB)*m_a+(col_index_B+2)]);
+			c_vec2=_mm_add_ps(_mm_mul_ps(a_vec,b_vec),c_vec2);
+
+			a_vec = _mm_loadu_ps(A+(n_a-iterator_index_AB)*m_a+row_index_A+8);
+			b_vec = _mm_set_ps1(B[(n_a-iterator_index_AB)*m_a+(col_index_B+2)]);
+			c_vec3=_mm_add_ps(_mm_mul_ps(a_vec,b_vec),c_vec3);
+		  }
+		}
+		_mm_storeu_ps(C+(col_index_B+2)*m_a+row_index_A,c_vec1);
+		_mm_storeu_ps(C+(col_index_B+2)*m_a+row_index_A+4,c_vec2);
+		_mm_storeu_ps(C+(col_index_B+2)*m_a+row_index_A+8,c_vec3);
+	  }
+	  if (col_remainder != 0) {
+		for (row_index_A = col_remainder;row_index_A != 0; row_index_A--){	
+		  c_dummy = 0;
+		  for (iterator_index_AB=0; iterator_index_AB < n_a; iterator_index_AB+=1){ //Reduce each block into c_vec
+			c_dummy += A[iterator_index_AB*m_a+m_a-row_index_A] * B[iterator_index_AB*m_a+(col_index_B+2)];  
+		  }
+		  C[(col_index_B+2)*m_a+m_a-row_index_A] = c_dummy; 
+		}
+	  } 
+
+
+	}
   }else if (m_a%2==0){
 	register int row_index_A, col_index_B=0, iterator_index_AB, max_size=m_a*n_a;
 	const register int col_remainder = m_a%(8), row_remainder = n_a%EDGE_SIZE_ROW;
 	register float c_dummy;
 	register __m128 a_vec, b_vec, c_vec1, c_vec2;
 
-	for (; col_index_B < m_a; col_index_B+=1){ //make sure index_B is the index of the beginning of each column of B
+	for (; col_index_B < m_a; col_index_B+=2){ //make sure index_B is the index of the beginning of each column of B
 	  for (row_index_A=0; row_index_A+7 < m_a; row_index_A+=8){ //Iterate accross each 4xm_a horizontal bock of A
 		c_vec1 = _mm_setzero_ps(); //Load C_Vec into memory
 		c_vec2 = _mm_setzero_ps(); //Load C_Vec into memory
@@ -289,6 +513,63 @@ void sgemm( int m_a, int n_a, float *A, float *B, float *C ) {
 		  C[col_index_B*m_a+m_a-row_index_A] = c_dummy; 
 		}
 	  } 
+
+
+
+
+	  for (row_index_A=0; row_index_A+7 < m_a; row_index_A+=8){ //Iterate accross each 4xm_a horizontal bock of A
+		c_vec1 = _mm_setzero_ps(); //Load C_Vec into memory
+		c_vec2 = _mm_setzero_ps(); //Load C_Vec into memory
+		for (iterator_index_AB=0; iterator_index_AB+EDGE_SIZE_ROW-1 < n_a; iterator_index_AB+=EDGE_SIZE_ROW){ //Reduce each block into c_vec
+		  a_vec = _mm_loadu_ps(A+iterator_index_AB*m_a+row_index_A);
+		  b_vec = _mm_set_ps1(B[iterator_index_AB*m_a+(col_index_B+1)]);
+		  c_vec1=_mm_add_ps(_mm_mul_ps(a_vec,b_vec),c_vec1);
+
+		  a_vec = _mm_loadu_ps(A+(iterator_index_AB+1)*m_a+row_index_A);
+		  b_vec = _mm_set_ps1(B[(iterator_index_AB+1)*m_a+(col_index_B+1)]);
+		  c_vec1=_mm_add_ps(_mm_mul_ps(a_vec,b_vec),c_vec1);
+
+		  a_vec = _mm_loadu_ps(A+(iterator_index_AB+2)*m_a+row_index_A);
+		  b_vec = _mm_set_ps1(B[(iterator_index_AB+2)*m_a+(col_index_B+1)]);
+		  c_vec1=_mm_add_ps(_mm_mul_ps(a_vec,b_vec),c_vec1);
+
+		  a_vec = _mm_loadu_ps(A+iterator_index_AB*m_a+row_index_A+4);
+		  b_vec = _mm_set_ps1(B[iterator_index_AB*m_a+(col_index_B+1)]);
+		  c_vec2=_mm_add_ps(_mm_mul_ps(a_vec,b_vec),c_vec2);
+
+		  a_vec = _mm_loadu_ps(A+(iterator_index_AB+1)*m_a+row_index_A+4);
+		  b_vec = _mm_set_ps1(B[(iterator_index_AB+1)*m_a+(col_index_B+1)]);
+		  c_vec2=_mm_add_ps(_mm_mul_ps(a_vec,b_vec),c_vec2);
+
+		  a_vec = _mm_loadu_ps(A+(iterator_index_AB+2)*m_a+row_index_A+4);
+		  b_vec = _mm_set_ps1(B[(iterator_index_AB+2)*m_a+(col_index_B+1)]);
+		  c_vec2=_mm_add_ps(_mm_mul_ps(a_vec,b_vec),c_vec2);
+		}
+		if (row_remainder != 0){
+		  for (iterator_index_AB=row_remainder; iterator_index_AB != 0; iterator_index_AB--){
+			a_vec = _mm_loadu_ps(A+(n_a-iterator_index_AB)*m_a+row_index_A);
+			b_vec = _mm_set_ps1(B[(n_a-iterator_index_AB)*m_a+(col_index_B+1)]);
+			c_vec1=_mm_add_ps(_mm_mul_ps(a_vec,b_vec),c_vec1);
+
+			a_vec = _mm_loadu_ps(A+(n_a-iterator_index_AB)*m_a+row_index_A+4);
+			b_vec = _mm_set_ps1(B[(n_a-iterator_index_AB)*m_a+(col_index_B+1)]);
+			c_vec2=_mm_add_ps(_mm_mul_ps(a_vec,b_vec),c_vec2);
+
+		  }
+		}
+		_mm_storeu_ps(C+(col_index_B+1)*m_a+row_index_A,c_vec1);
+		_mm_storeu_ps(C+(col_index_B+1)*m_a+row_index_A+4,c_vec2);
+	  }
+	  if (col_remainder != 0) {
+		for (row_index_A = col_remainder;row_index_A != 0; row_index_A--){	
+		  c_dummy = 0;
+		  for (iterator_index_AB=0; iterator_index_AB < n_a; iterator_index_AB+=1){ //Reduce each block into c_vec
+			c_dummy += A[iterator_index_AB*m_a+m_a-row_index_A] * B[iterator_index_AB*m_a+(col_index_B+1)];  
+		  }
+		  C[(col_index_B+1)*m_a+m_a-row_index_A] = c_dummy; 
+		}
+	  } 
+
 	}
   }else{ //Algorithm for odd indices
 	register int row_index_A, col_index_B=0, iterator_index_AB, max_size=m_a*n_a;
