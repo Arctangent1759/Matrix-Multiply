357a373ceae8b3d1a6968c9cdd80d13a05cd9bac
Arctangent1759 <alex.p.chu@gmail.com>
2013-08-03 16:43:08 -0700

    commit

--- sgemm-openmp.c
+++ sgemm-openmp.c
@@ -2,16 +2,21 @@
 #include <stdlib.h>
 #include <emmintrin.h>
 void sgemm( int m_a, int n_a, float *A, float *B, float *C ) {
-  register int col_index=0, row_index, col_offset;
-  __m128 a_vec, b_vec, c_vec;
-  for (col_index; col_index < n_a; col_index++){
-	for (row_index = 0; row_index < n_a; row_index++){
-	  b_vec = _mm_set_ps1(B[col_index*n_a+row_index]);
-	  for (col_offset = 0; col_offset < m_a; col_offset+=4){ //TODO: Edge case when m_a not a multiple of 4.
-		a_vec = _mm_loadu_ps(A+col_index*m_a+col_offset);
-		c_vec = _mm_loadu_ps(C+row_index*m_a+col_offset);
-		c_vec=_mm_add_ps(_mm_mul_ps(a_vec,b_vec),c_vec);
-		_mm_storeu_ps(C+row_index*m_a+col_offset,c_vec);
+  register int m_index, n_index, offset;
+  for (n_index=0; n_index<n_a; n_index++){
+	for (m_index=0; m_index<m_a; m_index++){
+	  register __m128 b_vec = _mm_load_ps1(B+n_index*m_a+m_index);
+	  #pragma omp for
+	  for (offset=0; offset+4<=m_a; offset+=4){
+		_mm_storeu_ps(
+		  C+n_index*m_a+offset,
+		  _mm_add_ps(
+			_mm_loadu_ps(C+n_index*m_a+offset),
+			_mm_mul_ps(
+			  _mm_loadu_ps(A+m_index*m_a+offset),
+			  b_vec)
+		  )
+		);
 	  }
 	}
   }
