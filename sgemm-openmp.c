5cbad4379eae5221a2149f43c4c29bfab4554cb2
Class Account <cs61c-hp@hive24.CS.Berkeley.EDU>
2013-08-03 20:23:04 -0700

    old implementation

--- sgemm-openmp.c
+++ sgemm-openmp.c
@@ -1,23 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <emmintrin.h>
+#include <omp.h>
+
+#define blocksize 16
+
 void sgemm( int m_a, int n_a, float *A, float *B, float *C ) {
-  register int m_index, n_index, offset;
-  for (n_index=0; n_index<n_a; n_index++){
-	for (m_index=0; m_index<m_a; m_index++){
-	  register __m128 b_vec = _mm_load_ps1(B+n_index*m_a+m_index);
-	  #pragma omp for
-	  for (offset=0; offset+4<=m_a; offset+=4){
-		_mm_storeu_ps(
-		  C+n_index*m_a+offset,
-		  _mm_add_ps(
-			_mm_loadu_ps(C+n_index*m_a+offset),
-			_mm_mul_ps(
-			  _mm_loadu_ps(A+m_index*m_a+offset),
-			  b_vec)
-		  )
-		);
+  int prod=m_a*n_a;
+  float *A_r = (float*)calloc(prod,sizeof(float));
+  float *B_c = (float*)calloc(prod,sizeof(float));
+  if (!A_r || !B_c){
+	printf("Memory Allocation failure.");
+	exit(1);
+  }
+  register int i,j,offset, shift_a, shift_b, shift_c;
+
+  //Convert A to shift_a major
+  for (i = 0; i < m_a; i++){
+	for (j = 0; j < n_a; j++){
+	  A_r[j+n_a*i]=A[i+m_a*j];
+	}
+  }
+  //Convert B to shift_b major
+  for (i = 0; i < n_a; i++){
+	for (j = 0; j < m_a; j++){
+	  B_c[i+n_a*j]=B[j+m_a*i];
+	}
+  }
+
+  //Multiply the matrices
+  for (j=0; j<m_a; j++){ //B index
+	for (i=0; i<m_a; i++){ //A index
+	  shift_a=j*n_a;
+	  shift_b=i*n_a;
+	  shift_c=j*m_a;
+	  for (offset=0; offset<n_a; offset++){
+		C[i+shift_c]+=A_r[i+shift_a]*B_c[j+shift_b];
 	  }
 	}
   }
+
+  //Free Memory
+  free(A_r);
+  free(B_c);
 }
